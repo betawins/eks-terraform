2e678411abad2d4b89cac9fb1147222322a47f90
hussain147 <hussainashrafi406@gmail.com>
2023-12-13 23:17:51 +0530

    updated

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ module "eks" {
   cluster_version = var.cluster_version
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
-  vpc_id          = "vpc-078adf04201af1076" # Replace With your VPC ID
-  subnet_ids      = ["subnet-0c8fd418db266da29","subnet-0aec3bd60c4eebfa1","subnet-0125ccbab90eb7efa"]  #Replace With your subnet IDs
+  vpc_id          = var.vpc_id
+  subnet_ids      = var.subnet_ids
 
   eks_managed_node_groups = var.eks_managed_node_groups
   eks_managed_node_group_defaults = var.eks_managed_node_group_defaults
--- variables.tf
+++ variables.tf
@@ -13,6 +13,21 @@ variable "cluster_version" {
   type        = string
   default     = "1.25"
 }
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+  default     = "vpc-078adf04201af1076"  # Replace with your VPC ID
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+  default     = [
+    "subnet-0c8fd418db266da29",
+    "subnet-0aec3bd60c4eebfa1",
+    "subnet-0125ccbab90eb7efa"
+  ]  # Replace with your subnet IDs
+}
 
 variable "eks_managed_node_groups" {
   description = "Map of EKS managed node group definitions to create"
